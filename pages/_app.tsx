d0453cf98ac152c94b033abd29241a4022c4417b
Your Name <enioluwaarowo@gmail.com>
2022-05-24 13:58:10 +0100

    adding enrollments and making some design changes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,20 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { NavBar } from '../components'
+import { UserProvider } from '../context'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <UserProvider>
+      <Head>
+        <title>E classroom</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <NavBar />
+      <Component {...pageProps} />
+    </UserProvider>
+  )
 }
 
 export default MyApp
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,86 +1,84 @@
+import axios from 'axios'
 import type { NextPage } from 'next'
-import Head from 'next/head'
 import Image from 'next/image'
-
+import { useContext, useEffect, useState } from 'react'
+import UserRoute from '../components/routes/UserRoute'
+import { UserContext } from '../context'
+import Enroll from '../components/Enroll'
+import Enrollments from '../components/Enrollments'
+import Link from 'next/link'
+import UserAuth from '../components/auth/UserAuth'
+interface Course {
+  name: String
+  category: String
+  description: String
+  lessons: []
+  published: boolean
+  createdAt: Date
+  updatedAt: Date
+  _id: any
+}
 const Home: NextPage = () => {
+  const {
+    state: { user },
+    dispatch,
+  } = useContext(UserContext)
+  const [courses, setCourses] = useState([])
+  const getCourses = async () => {
+    try {
+      await axios
+        .get(`${process.env.NEXT_PUBLIC_APP_NAME}/api/courses/published`)
+        .then(({ data }) => {
+          console.log(data)
+          setCourses(data)
+        })
+        .catch(({ response }) => {
+          console.log(response.data)
+        })
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  useEffect(() => {
+    getCourses()
+  }, [])
+  const photoUrl = `${process.env.NEXT_PUBLIC_APP_NAME}/api/posts/photo/`
+
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
-
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+    <div className="mt-[-48px] min-h-screen bg-purple-400 p-5 pt-16 md:p-10 md:pt-20 ">
+      {user && <Enrollments />}
+      <main className=" container mx-auto mt-10 rounded-2xl bg-gray-200 p-5">
+        <h1 className=" mb-5 text-3xl font-semibold">All Courses</h1>
+        <div className=" grid grid-cols-1 gap-6 md:grid-cols-12">
+          {courses?.map((course: Course, i) => {
+            return (
+              <div key={i} className=" col-span-1 md:col-span-6">
+                <div className=" relative h-60 w-full overflow-clip rounded-lg shadow">
+                  <Image
+                    src={`${process.env.NEXT_PUBLIC_APP_NAME}/api/course/image/${course._id}`}
+                    layout="fill"
+                    objectFit="cover"
+                    className=" bg-gray-400"
+                  />
+                  <div className="absolute bottom-0 left-0 right-0 flex h-20 items-center justify-between bg-gray-900 p-5 text-white opacity-80">
+                    <div>
+                      <Link href={`/teach/course/${course._id}`}>
+                        <a>{course.name}</a>
+                      </Link>
+                      <p>{course.category}</p>
+                    </div>
+                    <Enroll courseId={course?._id} />
+                  </div>
+                </div>
+              </div>
+            )
+          })}
         </div>
       </main>
-
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
-      </footer>
     </div>
   )
 }
 
 export default Home
+
+// export const getServerSideProps = UserAuth()
